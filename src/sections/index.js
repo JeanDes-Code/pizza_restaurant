c31fa444a2e059d619ac8240f7d8d2acbd0137b1
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-10-10 12:48:12 +0200

    Project init + base architecture

---
+++ src/sections/index.js
@@ -0,0 +1,5 @@
+export { default as AboutUs } from './AboutUs';
+export { default as FindUs } from './FindUs';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as OurMenu } from './OurMenu';
